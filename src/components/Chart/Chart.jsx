6de91427720730833c45b22f4e8354c3bab4cebc
João Borges Mendonça <jborgesmendonca@gmail.com>
2023-03-27 16:32:13 +0200

    master: Fixed chart.js versioning issue

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,4 +1,6 @@
 import { Line } from "react-chartjs-2";
+// Even though "import { Chart as ChartJS} from "chart.js/auto";" is not being used under, it is required for the Chart.js work.
+// eslint-disable-next-line
 import { Chart as ChartJS} from "chart.js/auto";
 
 function Chart ({chartData}) {
